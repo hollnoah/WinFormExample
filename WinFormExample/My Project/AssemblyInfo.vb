82b297f5d9a39992c444fcfbf64e50a08fba959a
Noah Holloway <noahholloway23@gmail.com>
2025-03-03 08:11:27 -0700

    Add project files.

---
+++ WinFormExample/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("WinFormExample")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Razer")>
+<Assembly: AssemblyProduct("WinFormExample")>
+<Assembly: AssemblyCopyright("Copyright © Razer 2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("29f1236d-971a-4781-b7c6-8fddfeff4551")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
